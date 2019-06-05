3efa0555775612f2cff319c010bf623b54d09afb
Sofía Celi <cherenkovd69@gmail.com>
2019-06-05 23:16:33 +0200

    Add multiplication and substration

--- operations.go
+++ operations.go
@@ -5,3 +5,15 @@ type number int
 func (x number) Add(y number) number {
 	return x + y
 }
+
+func substraction(a, b int) int {
+	return a - b
+}
+
+func (x number) substr(b number) number {
+	return x - b
+}
+
+func (x number) Multiplication(y number) number {
+	return x * y
+}
--- operations_test.go
+++ operations_test.go
@@ -19,3 +19,34 @@ func (s *OperationsSuite) Test_Add(c *C) {
 	z = x.Add(y)
 	c.Assert(z, Equals, number(8))
 }
+
+func (s *OperationsSuite) Test_Substract(c *C) {
+	a := 8
+	b := 9
+	d := substraction(a, b)
+	c.Assert(a-b, Equals, d)
+}
+
+func (s *OperationsSuite) Test_Substr(c *C) {
+	var a, b, d number
+	a = 8
+	b = 9
+	d = a.substr(b)
+	c.Assert(a-b, Equals, d)
+}
+
+func (s *OperationsSuite) Test_Multiplication(c *C) {
+	var x, y, z number
+	x = 2
+	y = 8
+	z = x.Multiplication(y)
+	c.Assert(z, Equals, number(16))
+}
+
+func (s *OperationsSuite) Test_Double_Multiplication(c *C) {
+	var x, y, z number
+	x = 2
+	y = 8
+	z = x.Multiplication(y).Multiplication(10)
+	c.Assert(z, Equals, number(160))
+}
